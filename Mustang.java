a7c6c512118b0191f8db7dffab5385c1f6a058af
Jacky Chen <40508134+hfbgk321@users.noreply.github.com>
2018-11-18 16:31:50 -0800

    11-18-18: HorseBarn Project

---
+++ Mustang.java
@@ -0,0 +1,12 @@
+public class Mustang implements Horse{
+    private String name;
+    private int weight;
+
+    public Mustang(String name){
+        Super("Mustang");
+        this.name = name;
+        this.weight =weight;
+    }
+    public String getName();
+    public int getWeight();
+}
