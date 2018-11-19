ae1a87de265a7e902c3f59f4793ee20d3764438c
BT_1N3_01 <jchen4465@bths.edu>
2018-11-19 08:24:14 -0500

    11-19-18: HorseBarn

--- src/Mustang.java
+++ src/Mustang.java
@@ -2,6 +2,7 @@ public class Mustang implements Horse{
     private String name;
     private int weight;
 
+
     public Mustang(String name, int weight){
         this.name = name;
         this.weight = weight;
